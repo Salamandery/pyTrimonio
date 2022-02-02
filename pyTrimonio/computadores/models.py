f96dcc15c4b736afbadde784733f152f6810d158
salamandery <sp.ront@hotmail.com>
2022-02-02 14:11:22 -0300

    a link target

--- pyTrimonio/computadores/models.py
+++ pyTrimonio/computadores/models.py
@@ -8,7 +8,7 @@ class Computador(models.Model):
     hostname = models.CharField('Nome do Computador', 
         max_length=50, blank=True, null=True
     )
-    ip = models.CharField('IP', max_length=12, blank=True, null=True)
+    ip = models.CharField('IP', max_length=16, blank=True, null=True)
     mac = models.CharField('MAC', max_length=18, blank=True, null=True)
     mark = models.CharField('Marca', max_length=100)
     model = models.CharField('Modelo', max_length=100, blank=True, null=True)
