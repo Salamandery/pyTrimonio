4315fb447693a37f9628a70e6e9a655c98fe5927
salamandery <sp.ront@hotmail.com>
2022-06-13 14:05:43 -0300

    patrimonio

--- pyTrimonio/impressoras/admin.py
+++ pyTrimonio/impressoras/admin.py
@@ -4,11 +4,11 @@ from .models import Impressora
 # Register your models here.
 class ImpressoraAdmin(admin.ModelAdmin):
     list_display = [
-        'hostname', 'ip',
+        'hostname', 'ip', 'identified',
         'mark', 'sector', 
         'location', 'updated_at'
     ]
-    search_fields = ['mark', 'sector', 'ip', 'hostname', 'location']
+    search_fields = ['mark', 'sector', 'ip', 'hostname', 'location', 'identified']
     prepopulated_fields = {'description': ('hostname','mark',)}
 
 admin.site.register(Impressora, ImpressoraAdmin)
